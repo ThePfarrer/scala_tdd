f64007e792a4aae49460921b877d3a90f0dc917b
Uche Okereke <orezpablo@gmail.com>
2022-09-15 22:55:51 +0200

    change null to option type

--- src/test/scala/support/steps/BaseCoversionSteps.scala
+++ src/test/scala/support/steps/BaseCoversionSteps.scala
@@ -4,14 +4,16 @@ import com.thepfarrer.{BaseConversion, Binary, Decimal, Number}
 
 class BaseCoversionSteps extends BaseSteps {
   var baseFrom: Option[Number] = None
-  var baseTo: Option[Number] = None
+  var baseTo: Option[Number]   = None
 
   Given("""^I have a (binary|decimal) number (\d+)$""") { (x: String, baseFromNumber: String) =>
     baseFrom = Some(if (x == "binary") Binary(baseFromNumber) else Decimal(baseFromNumber))
   }
 
   When("""^I convert it to (binary|decimal) using the BaseConversion utility$""") { (x: String) =>
-    baseTo = Some(if (x == "binary") BaseConversion.decimalToBinary(baseFrom.get) else BaseConversion.binaryToDecimal(baseFrom.get))
+    baseTo = Some(
+      if (x == "binary") BaseConversion.decimalToBinary(baseFrom.get) else BaseConversion.binaryToDecimal(baseFrom.get)
+    )
   }
 
   Then("""^I get back a (binary|decimal) number (\d+)$""") { (x: String, expected: String) =>
